6c8be215fadfeae1bcf4893369e5f2f6b91c88e4
MatejKozica <v-matej.kozica@dump.hr>
2017-08-10 20:02:12 +0200

    Inicijalizacija filea i stvaranje Animal classe

---
+++ Animal/Animal/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public string Name;
+        public string Sound;
+        public string Species;s
+    }
+}
